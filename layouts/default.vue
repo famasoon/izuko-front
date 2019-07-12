7325a6d53a3d3372b7e70a8c3c41d51264764ea8
famasoon <famasoon@gmail.com>
2019-07-13 00:37:03 +0900

    Clear template index

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,9 @@
 <template>
   <div>
-    <nav-bar></nav-bar>
-
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-    </section>
+    <nav-bar />
+    <div class="container column is-10">
+      <nuxt />
+    </div>
   </div>
 </template>
 
@@ -35,22 +13,6 @@ import NavBar from '~/components/NavBar'
 export default {
   components: {
     NavBar
-  },
-  data() {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
-    }
   }
 }
 </script>
