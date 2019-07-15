f84601966fe430cdb702c7212f82c8d7fa6109fb
famasoon <famasoon@gmail.com>
2019-07-15 19:30:39 +0900

    Add description on index.vue

---
+++ components/Description.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="media">
+    <div class="media-content">
+      <p class="content">
+        <strong>IZUKO</strong> show the following information
+      </p>
+      <b-table :data="data" :columns="columns" />
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      data: [
+        { id: 1, information: 'Whois' },
+        { id: 2, information: 'IP Address' },
+        { id: 3, information: 'Name Servers' },
+        { id: 4, information: 'MX Records' }
+      ],
+      columns: [
+        {
+          field: 'id',
+          label: '*',
+          width: '40',
+          numeric: true
+        },
+        {
+          field: 'information',
+          label: 'Information'
+        }
+      ]
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,20 @@
     <div class="columns is-mobile" />
     <hero />
     <input-domain-form />
+    <description />
   </section>
 </template>
 
 <script>
 import Hero from '~/components/Hero.vue'
 import InputDomainForm from '~/components/InputDomainForm.vue'
+import Description from '~/components/Description.vue'
+
 export default {
   components: {
     Hero,
-    InputDomainForm
+    InputDomainForm,
+    Description
   }
 }
 </script>
