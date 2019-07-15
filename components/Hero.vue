1af70f4c0e70fc134800adb0839b9fc1e3df5cea
famasoon <famasoon@gmail.com>
2019-07-15 19:07:53 +0900

    Fix some url

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,14 +1,14 @@
 <template>
-        <section class="hero is-medium is-primary is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            izuko.ch
-          </h1>
-          <h2 class="subtitle">
-            izuko.ch is show the information for OSINT
-          </h2>
-        </div>
+  <section id="hero-section" class="hero is-medium is-primary is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          <strong class="has-text-success">Welcome</strong> to izuko.ch
+        </h1>
+        <h2 class="subtitle">
+          izuko.ch show the information for OSINT
+        </h2>
       </div>
-    </section>
+    </div>
+  </section>
 </template>
