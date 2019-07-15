887a30fed078bfcefbf86a6b39f85916d3a86b74
famasoon <famasoon@gmail.com>
2019-07-15 19:08:37 +0900

    Fix urls on NavBar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,7 +1,8 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
+    <div class="navbar-brand" />
+    <div class="navbar-start">
+      <a class="navbar-item" href="https://izuko.ch">
         <p>izuko.ch</p>
       </a>
     </div>
