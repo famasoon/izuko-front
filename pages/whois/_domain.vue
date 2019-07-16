f0712b1ea43d2e7971dcf2611c28034051c5e86c
famasoon <famasoon@gmail.com>
2019-07-16 22:48:02 +0900

    Display whois information of each domain using API

--- pages/whois/_domain.vue
+++ pages/whois/_domain.vue
@@ -1,3 +1,41 @@
 <template>
-  <div>{{ $route.params.domain }}</div>
+  <div>
+    <h1 class="title">
+      {{ $route.params.domain }}
+    </h1>
+    <h2 class="subtitle">
+      Related information
+    </h2>
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          Whois
+        </p>
+        <a href="#" class="card-header-icon" aria-label="more options">
+          <span class="icon">
+            <i class="fas fa-angle-down" aria-hidden="true" />
+          </span>
+        </a>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          <pre>{{ resp.WhoisResult }}</pre>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData(context) {
+    const DOMAIN_NAME = context.params.domain
+    const { data } = await axios.get(
+      'http://izuko.ch/api/whois/' + DOMAIN_NAME
+    )
+    return { resp: data }
+  }
+}
+</script>
