4635a68cb6aab420ae86fe84962ac0a81a9df4bc
famasoon <famasoon@gmail.com>
2019-07-16 21:23:26 +0900

    Support nuxt-link

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,42 @@
   <section class="section">
     <div class="columns is-mobile" />
     <hero />
-    <input-domain-form />
+    <div id="inputfield">
+      <b-field position="is-centered">
+        <b-input v-model="domain" placeholder="Input Domain or IP address..." type="search" icon="magnify" />
+        <nuxt-link :to="{ name: 'whois-domain', params: { domain: domain }}">
+          <p class="control">
+            <button class="button is-info">
+              Lookup
+            </button>
+          </p>
+        </nuxt-link>
+      </b-field>
+    </div>
     <description />
   </section>
 </template>
 
 <script>
 import Hero from '~/components/Hero.vue'
-import InputDomainForm from '~/components/InputDomainForm.vue'
 import Description from '~/components/Description.vue'
 
 export default {
   components: {
     Hero,
-    InputDomainForm,
     Description
+  },
+
+  data() {
+    return {
+      domain: ''
+    }
   }
 }
 </script>
+
+<style>
+#inputfield {
+  margin: 40px;
+}
+</style>
---
+++ pages/whois/_domain.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>{{ $route.params.domain }}</div>
+</template>
